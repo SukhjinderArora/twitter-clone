7c9e4a811952a9e98fe06223aacb85042b1c689b
Sukhjinder Arora <35064725+SukhjinderArora@users.noreply.github.com>
2022-08-05 19:19:41 +0530

    refactor: added event based prisma client logging to log all SQL queries to terminal

--- api/services/connect-db.js
+++ api/services/connect-db.js
@@ -1,5 +1,19 @@
 const { PrismaClient } = require('@prisma/client');
 
-const prisma = new PrismaClient();
+const logger = require('../utils/logger');
+
+const prisma = new PrismaClient({
+  log: [
+    {
+      emit: 'event',
+      level: 'query',
+    },
+  ],
+});
+
+prisma.$on('query', (e) => {
+  logger.info(`Query: ${e.query}`);
+  logger.info(`Duration: ${e.duration} ms`);
+});
 
 module.exports = prisma;
