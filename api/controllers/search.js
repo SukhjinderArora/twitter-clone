8b2d991cdf58d744cd117c96b8d609c533deb4a0
Sukhjinder Arora <35064725+SukhjinderArora@users.noreply.github.com>
2022-08-06 18:13:03 +0530

    feat: added api to search users by name

--- api/controllers/search.js
+++ api/controllers/search.js
@@ -46,7 +46,55 @@ const searchUsersByUsername = async (req, res, next) => {
   }
 };
 
-const searchUsersByName = () => {};
+const searchUsersByName = async (req, res, next) => {
+  const { name } = req.params;
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 10;
+
+  try {
+    const total = await prisma.user.count({
+      where: {
+        profile: {
+          name: {
+            startsWith: name,
+            mode: 'insensitive',
+          },
+        },
+      },
+    });
+    const users = await prisma.user.findMany({
+      where: {
+        profile: {
+          name: {
+            startsWith: name,
+            mode: 'insensitive',
+          },
+        },
+      },
+      select: {
+        username: true,
+        profile: {
+          select: {
+            name: true,
+            img: true,
+          },
+        },
+      },
+      skip: (page - 1) * limit,
+      take: limit,
+    });
+    return res.status(200).json({
+      info: {
+        total,
+        nextPage: total > (page - 1) * limit + users.length ? page + 1 : null,
+        prevPage: page === 1 ? null : page - 1,
+      },
+      results: users,
+    });
+  } catch (error) {
+    return next(error);
+  }
+};
 
 module.exports = {
   searchUsersByUsername,
