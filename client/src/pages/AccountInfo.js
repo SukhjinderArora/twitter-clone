d8f87d0deaad68bfca138d467232f0bcfd1b0bc5
Sukhjinder Arora <35064725+SukhjinderArora@users.noreply.github.com>
2022-09-09 15:58:56 +0530

    feat: fetching account info from backend using react-query

--- client/src/pages/AccountInfo.js
+++ client/src/pages/AccountInfo.js
@@ -2,12 +2,38 @@
 import { RiArrowLeftLine, RiArrowRightSLine } from 'react-icons/ri';
 import { IconContext } from 'react-icons';
 import { NavLink, useNavigate } from 'react-router-dom';
+import { useQuery } from 'react-query';
+
+import axios from '../utils/axios';
+import dayjs from '../utils/day';
 
 import usePageTitle from '../hooks/usePageTitle';
+
+import Spinner from '../components/Spinner';
 
 const AccountInfo = () => {
   usePageTitle('Account information / Kookoo');
   const navigate = useNavigate();
+  const { data, isLoading, isError } = useQuery(
+    'account',
+    async () => {
+      const response = await axios.get('/api/users/me');
+      return response.data;
+    },
+    {
+      refetchOnWindowFocus: false,
+    }
+  );
+
+  if (isLoading)
+    return (
+      <div className="w-full h-full flex items-center justify-center">
+        <Spinner />
+      </div>
+    );
+
+  if (isError) return <div>Something went wrong.</div>;
+
   return (
     <div>
       <div className="px-4 py-4 sticky top-0">
@@ -42,7 +68,7 @@ const AccountInfo = () => {
                 <div>
                   <p className="text-on-surface">Username</p>
                   <p className="text-sm text-on-surface/70 -mt-1">
-                    @_sukh_arora
+                    @{data.user.username}
                   </p>
                 </div>
                 <div className="text-on-surface/80">
@@ -66,7 +92,7 @@ const AccountInfo = () => {
                 <div>
                   <p className="text-on-surface">Email</p>
                   <p className="text-sm text-on-surface/70 -mt-1">
-                    test@test.com
+                    {data.user.email}
                   </p>
                 </div>
                 <div className="text-on-surface/80">
@@ -90,7 +116,7 @@ const AccountInfo = () => {
                 <div>
                   <p className="text-on-surface">Birth date</p>
                   <p className="text-sm text-on-surface/70 -mt-1">
-                    Jan 1, 1995
+                    {dayjs(data.user.profile.dob).format('MMM D, YYYY')}
                   </p>
                 </div>
                 <div className="text-on-surface/80">
@@ -109,28 +135,24 @@ const AccountInfo = () => {
             </NavLink>
           </li>
           <li className="pb-4">
-            <NavLink to="/">
-              <div className="flex justify-between items-center px-4">
-                <div>
-                  <p className="text-on-surface">Account creation</p>
-                  <p className="text-sm text-on-surface/70 -mt-1">
-                    Jun 6, 2020, 8:45 PM
-                  </p>
-                </div>
-                <div className="text-on-surface/80">
-                  <IconContext.Provider
-                    value={{
-                      size: '24px',
-                      style: {
-                        color: 'inherit',
-                      },
-                    }}
-                  >
-                    <RiArrowRightSLine />
-                  </IconContext.Provider>
-                </div>
+            <div className="flex justify-between items-center px-4">
+              <div>
+                <p className="text-on-surface">Account creation</p>
+                <p className="text-sm text-on-surface/70 -mt-1">
+                  {dayjs(data.user.createdAt).format('MMM D, YYYY, h:mm:ss A')}
+                </p>
               </div>
-            </NavLink>
+            </div>
+          </li>
+          <li className="pb-4">
+            <div className="px-4">
+              <div>
+                <p className="text-on-surface">Age</p>
+                <p className="text-sm text-on-surface/70 -mt-1">
+                  {dayjs().diff(dayjs(data.user.profile.dob), 'year')}
+                </p>
+              </div>
+            </div>
           </li>
         </ul>
       </nav>
